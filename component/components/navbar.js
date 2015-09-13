0e2418d8da728ff99d5a2c0afd65744333b9720c
Alexius Wronka <awronka@hotmail.com>
2015-09-13 19:11:55 -0400

    navbar started it says navbar

---
+++ component/components/navbar.js
@@ -0,0 +1,15 @@
+var React = require( 'react' );
+
+
+var NavBar = React.createClass( {
+	displayName: 'NavBar',
+	render: function() {
+		return (
+			<div>
+				<p>NavBar</p>
+			</div>
+		);
+	}
+} );
+
+module.exports = NavBar;
